db9247f25d3b2672f757762ef70a0fd3a1ca0a76
Tatsuki-I <tatsuki.devel@gmail.com>
2019-08-05 17:23:39 +0900

    最終的なhaddockコメントを追記

--- src/Minfree.hs
+++ src/Minfree.hs
@@ -14,16 +14,22 @@ import Data.Array.ST (runSTArray, newArray, writeArray)
 minfree :: [Int] -> Int
 minfree xs = head ([0..] \\ xs)
 
+-- | リスト us から vs に含まれる要素をすべて除いた残りの要素のリストを返す
 (\\) :: Eq a => [a] -> [a] -> [a]
 us \\ vs = filter (`notElem` vs) us
 
+-- |
+-- 引数として論理値の配列を取る
+-- 論理値のリストに変換して True エントリーからなる最長先頭部分列の長さを返す
 search :: Array Int Bool -> Int
 search = length . takeWhile id . elems
 
+-- | リストから配列への変換
 checklist :: [Int] -> Array Int Bool
 checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) xs) (repeat True))
   where n = length xs
 
+-- | checklist の置き換え
 countlist :: [Int] -> Array Int Int
 countlist xs = accumArray (+) 0 (0,n) (zip xs (repeat 1))
   where n = maximum xs
@@ -31,6 +37,7 @@ countlist xs = accumArray (+) 0 (0,n) (zip xs (repeat 1))
 sort :: [Int] -> [Int]
 sort xs = concat [replicate k x | (x,k) <- assocs (countlist xs)]
 
+-- | Data.Array.ST モジュールを使った checklist
 checklist' :: [Int] -> Array Int Bool
 checklist' xs = runSTArray $ do
   a <- newArray (0, n) False
@@ -38,12 +45,15 @@ checklist' xs = runSTArray $ do
   return a
   where n = length xs
 
+-- | リストを述語 p を満たす要素と満たさない要素のリストに分割する
 partition :: (Int -> Bool) -> [Int] -> ([Int], [Int])
 partition p xs = (filter p xs, filter (not . p) xs)
 
+-- | 最終的な minfree
 minfree' :: [Int] -> Int
 minfree' xs = minfrom 0 (length xs, xs)
 
+-- | minfree の一般化
 minfrom :: Int -> (Int, [Int]) -> Int
 minfrom a (n, xs)
   | n == 0 = a
